618b91ef778ff8d04d2fc2426156c8d62918c218
Vanilla Black <vanilla@f66.dev>
2023-01-04 21:24:14 -0500

    Remove hard-coded canvas codes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,6 @@
     export let data;
 
     const version = 'v' + data.version;
-
-    const finishedCanvases = ('1,2,3,4,5,6,7,8,9,10,11,12,13,13b,14,15,16,17,' +
-    '18,19,20,21,21a,22,23,24,25,26,27,28,29,30,30a,31,32,33,34,34a,35,36,' +
-    '37,38,39,40,41,42,43,43a,44,45,45a,46,47,48,49,50,51,52,53,54,55,56,' +
-    '56a,57,58,59,60,60a,61,62,63').split(',');
-    const activeCanvas = '64';
 </script>
 
 <svelte:head>
@@ -30,9 +24,8 @@
         <br />
         <p><b>Clicking on an image will open it in a new tab</b></p>
     </div>
-    <CanvasDetails canvas={activeCanvas} meta={data.canvases[activeCanvas]} active />
-    {#each [...finishedCanvases].reverse() as canvas}
-        <CanvasDetails canvas={canvas} meta={data.canvases[canvas]} />
+    {#each [...data.canvases].reverse() as canvas}
+        <CanvasDetails canvas={canvas.code} meta={canvas} active={canvas.active} />
     {/each}
 </div>
 
